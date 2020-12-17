d1e8c177b0b82fa0db5310ab8e8303dc8ddc1d7d
ChihYuanWang <ben0917460069@hotmail.com>
2020-12-17 20:24:12 +0800

    修復LiveData 擴充 Event 異常

--- KotlinHelper/src/main/java/com/jrtou/kotlinhelper/livedata/Event.kt
+++ KotlinHelper/src/main/java/com/jrtou/kotlinhelper/livedata/Event.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.Observer
 open class Event<out T>(private val data: T) {
     //標記是否已被UI使用
     var hasBeenHandled = false
-    fun getDataIfNotHandled(): T? = if (hasBeenHandled) null else data
+    fun getDataIfNotHandled(): T? = if (hasBeenHandled) null else {
+        hasBeenHandled = true
+        data
+    }
 
     fun getData(): T = data
 }
